529358f6bf909830462d07d619197416bfbc9ba1
Felipe Nicholson <fnicholson93@gmail.com>
2017-09-27 10:28:50 -0300

    Auto stash before merge of "master" and "origin/master"

--- exito.php
+++ exito.php
@@ -1,3 +1,14 @@
-<?php
-include('helpers.php');
- ?>
+
+    <div class="felicitaciones alert-success">
+      <strong>¡FELICITACIONES!</strong>  <a href="#" class="alert-link"></a>
+      <br>
+      <p> Ingresaste con exito. </p>
+    </div>
+
+<button type="button" class="btn btn-default">Volver a la página de inicio</button>
+
+  </body>
+
+  
+
+</html>
