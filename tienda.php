07972326e0cc81e3cce25fd86f0b27d029775186
Unknown <paumonteiro@hotmail.com>
2017-11-14 19:26:55 -0300

    Tienda: Pag. en Contrucción

---
+++ tienda.php
@@ -0,0 +1,21 @@
+<?php
+include('soporte.php');
+include("header.php");
+?>
+
+      <div class="col-xs-12">
+        <br><br>
+        <h1><strong>Pagina en Construcción :|</strong></h1>
+        <br>
+        <div class="row centrado">
+          <p>Disculpe las molestias. Gracias, vuelva prontos.</p>
+          <br>
+          <a href="index.php" type="button" class="btn btn-default-2">Home</a>
+        </div>
+      </div>
+
+
+    </div>
+  </body>
+<!-- FOOTER -->
+<?php include("footer.php"); ?>
