3cb5dc10b9bc6cea1b258491579043f2cd941a0f
Felipe Nicholson <fnicholson93@gmail.com>
2017-11-22 10:17:04 -0300

    Auto stash before merge of "master" and "origin/master"

--- tienda.php
+++ tienda.php
@@ -3,12 +3,59 @@ include('soporte.php');
 include("header.php");
 ?>
 
-      <div class="col-xs-12">
+
+      <div class="col-md-12">
         <br><br>
-        <h1><strong>Pagina en Construcción :|</strong></h1>
-        <br>
+        <h1><strong>Productos</strong></h1>
+
+        <div class="row">
+          <div class="box-producto col-md-4">
+            <img src="img/box-producto1.jpg" alt="foto-izq" class="imagen">
+            <h2 class="titulo"> Campera Vintage I </h2>
+            <br>
+            <button type="button" name="button">Ir a producto</button>
+          </div>
+          <div class="box-producto col-md-4">
+            <img src="img/box-producto2.jpg" alt="foto-izq" class="imagen">
+            <h2 class="titulo"> Campera Vintage II </h2>
+            <br>
+            <button type="button" name="button">Ir a producto</button>
+          </div>
+          <div class="box-producto col-md-4">
+            <img src="img/box-producto3.jpg" alt="foto-izq" class="imagen">
+            <h2 class="titulo"> Campera Vintage III </h2>
+            <br>
+            <button type="button" name="button">Ir a producto</button>
+          </div>
+        </div>
+
+        <div class="row"> <!--segunda fila -->
+          <div class="box-producto col-md-4">
+            <img src="img/box-producto5.jpg" alt="foto-izq" class="imagen">
+            <h2 class="titulo"> Campera Bomber I </h2>
+            <br>
+            <button type="button" name="button">Ir a producto</button>
+          </div>
+          <div class="box-producto col-md-4">
+            <img src="img/box-producto4.jpg" alt="foto-izq" class="imagen">
+            <h2 class="titulo"> Campera Vintage IV </h2>
+            <br>
+            <button type="button" name="button">Ir a producto</button>
+          </div>
+          <div class="box-producto col-md-4">
+            <img src="img/box-producto6.jpg" alt="foto-izq" class="imagen">
+            <h2 class="titulo"> Campera Bomber II </h2>
+            <br>
+            <button type="button" name="button">Ir a producto</button>
+          </div>
+        </div>
+
+
+
+
+
         <div class="row centrado">
-          <p>Disculpe las molestias. Gracias, vuelva prontos.</p>
+
           <br>
           <a href="index.php" type="button" class="btn btn-default-2">Home</a>
         </div>
